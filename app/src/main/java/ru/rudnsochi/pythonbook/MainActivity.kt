5177a66d1313ea0ceec45d8a25264b3e4086df48
MolotkovD <64916506+MolotkovD@users.noreply.github.com>
2024-04-14 10:50:59 +0300

    loginc

--- app/src/main/java/ru/rudnsochi/pythonbook/MainActivity.kt
+++ app/src/main/java/ru/rudnsochi/pythonbook/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.rudnsochi.pythonbook
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.webkit.WebView
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +10,15 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val drawerLayout : DrawerLayout = findViewById(R.id.main)
+        val mainWeb : WebView = findViewById(R.id.mainWeb)
 
 
         val toolBar : Toolbar = findViewById(R.id.headtoolbar)
@@ -22,11 +27,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val nav = findViewById<NavigationView>(R.id.nav_view)
         nav.setNavigationItemSelectedListener(this)
 
-        val toogle = ActionBarDrawerToggle(this, drawerLayout,
-            toolBar, R.string.open_nav, R.string.close_nav)
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolBar, R.string.open_nav, R.string.close_nav)
 
-        drawerLayout.addDrawerListener(toogle)
-        toogle.syncState()
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        mainWeb.loadUrl("file:///android_asset/Глава 1. Введение в язык Kotlin/Первая программа на Kotlin в IntelliJ IDEA.html")
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
